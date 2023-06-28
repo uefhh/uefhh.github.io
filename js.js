b85a784b64721435e8f829d858b547a5ddf02d87
uefhh <131259668+uefhh@users.noreply.github.com>
2023-06-28 20:36:01 +0800

    Update js.js
    1

--- js.js
+++ js.js
@@ -4,3 +4,16 @@ const text = document.querySelector('.text');
 heart.addEventListener('click', function() {
   text.style.opacity = 1;
 });
+
+var fileButton = document.getElementById('fileButton');
+fileButton.addEventListener('click', function() {
+  window.location.href = '1.html';
+});
+
+
+
+var myButton = document.getElementById('myButton');
+myButton.addEventListener('click', function() {
+  console.log(1)
+  window.location.href = 'https://b23.tv/Zn3Do37';
+});
